a230b413e50d34b8fb3ce32c2946da94c49745b3
shibu42yaaaa <shibu.4242git.formac@gmail.com>
2023-03-03 21:20:23 +0900

    勇者クラス必殺技追加

--- techpit/Ruby_ドラクエ風アプリ/dorakue.rb
+++ techpit/Ruby_ドラクエ風アプリ/dorakue.rb
@@ -3,6 +3,9 @@ class Brave
   attr_reader :name, :offense, :defense
   attr_accessor :hp
 
+  # 必殺技
+  SPECIAL_ATTACK_CONSTANT = 1.5
+
   def initialize(**params)
     @name = params[:name]
     @hp = params[:hp]
@@ -11,13 +14,29 @@ class Brave
   end
 
   def attack(monster)
-    damage = @offense - monster.defense
+    puts "#{@name}の攻撃"
+
+    # 0~3の間でランダム数値
+    attack_num = rand(4)
+
+    # 4分の1の確率でspecial_attackを実行
+    if attack_num == 0
+      puts "必殺技"
+      damage = culculate_special_attack - monster.defense
+    else
+      puts "通常攻撃"
+      damage = @offense - monster.defense
+    end
+
     monster.hp -= damage
     
     # メッセージ追加
-    puts "#{@name}の攻撃"
     puts "#{monster.name}は#{damage}ポイントのダメージを受けた。"
     puts "#{monster.name}の残りHPは#{monster.hp}ポイントだ。"
+  end
+
+  def culculate_special_attack
+    (@offense * SPECIAL_ATTACK_CONSTANT).round
   end
 end
 
